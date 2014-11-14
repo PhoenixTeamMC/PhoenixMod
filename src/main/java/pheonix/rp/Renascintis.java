ed769c4eebe8067dfae3c64574c5920e027ccaf5
chbachman <cbachman@bachmangroup.com>
2014-11-14 08:00:35 -0600

    Added B/Gold Text

--- src/main/java/pheonix/rp/Renascintis.java
+++ src/main/java/pheonix/rp/Renascintis.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import pheonix.lib.item.ItemBase;
 import pheonix.main.IIntializer;
@@ -43,11 +45,16 @@ public class Renascintis implements IIntializer{
 
 	public class ItemRP extends ItemBase{
 		
+		public ItemRP(){
+			this.setMaxStackSize(1);
+		}
 		
 		public EnumRarity getRarity(ItemStack stack){
 			return EnumRarity.epic;
 		}
 
+		
+		
 		@Override
 		public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
 			
@@ -55,15 +62,22 @@ public class Renascintis implements IIntializer{
 				String name = super.getRawName(stack);
 				
 				if(name.equals("sol")){
-					player.addChatMessage(new ChatComponentText("CALZ CONPHO SOBRA ZOL ROR I TA NAZPSAD"));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.ITALIC.toString() + StatCollector.translateToLocal("info.pheonix.whisper")));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD.toString() + EnumChatFormatting.GOLD.toString() + "CALZ CONPHO SOBRA ZOL ROR I TA NAZPSAD"));
 				}else if(name.equals("luna")){
-					player.addChatMessage(new ChatComponentText("GRAA TA MALPRG DS HOLQ NOTHOA ZIMZ"));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.ITALIC.toString() + StatCollector.translateToLocal("info.pheonix.whisper")));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD.toString() + EnumChatFormatting.GOLD.toString() + "GRAA TA MALPRG DS HOLQ NOTHOA ZIMZ"));
 				}
 				
 				
 			}
 			
 			return super.onItemRightClick(stack, world, player);
+		}
+
+		@Override
+		public boolean hasEffect(ItemStack par1ItemStack, int pass) {
+			return true;
 		}
 		
 		
