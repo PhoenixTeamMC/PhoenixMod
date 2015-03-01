874008e1781eff30ba68959c02b3adddc45fe72c
Strikingwolf <Strikingwolf2012@gmail.com>
2015-02-28 20:43:28 -0600

    Fix version num in Reference

--- src/main/java/phoenix/main/lib/Reference.java
+++ src/main/java/phoenix/main/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "PhoenixMod";
 	public static final String MODNAME = "Phoenix Mod";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 	public static final String DEPENDENCIES = "";
 
 }
