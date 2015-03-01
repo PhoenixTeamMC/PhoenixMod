2216ca38c4b3ab0effb1fb918b167c3581931f52
Strikingwolf <Strikingwolf2012@gmail.com>
2015-02-28 22:59:59 -0600

    Add logging and make it save data

--- src/main/java/phoenix/pathfinder/PathHandler.java
+++ src/main/java/phoenix/pathfinder/PathHandler.java
@@ -1,5 +1,6 @@
 package phoenix.pathfinder;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
@@ -7,19 +8,70 @@ import net.minecraft.world.EnumDifficulty;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 public class PathHandler {
 
-	public HashMap<Long, Boolean> not_done = new HashMap<Long, Boolean>();
+	static {
+		try
+		{
+			FileInputStream fileIn = new FileInputStream("/pathfinder/not_done.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			HashMap<Long, Boolean> hash = (HashMap<Long, Boolean>) in.readObject();
+			in.close();
+			fileIn.close();
+			if (hash == null) { HashMap<Long, Boolean> not_done = new HashMap<Long, Boolean>();}
+		}catch(IOException i)
+		{
+			HashMap<Long, Boolean> not_done = new HashMap<Long, Boolean>();		}catch(ClassNotFoundException c)
+		{
+			HashMap<Long, Boolean> not_done = new HashMap<Long, Boolean>();		}
+	}
+
+	private HashMap<Long, Boolean> getNot_Done() {
+		try {
+			FileInputStream fileIn = new FileInputStream("/pathfinder/not_done.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			Object o = in.readObject();
+			if (o instanceof HashMap) {
+				return (HashMap<Long, Boolean>) o;
+			} else {
+				return new HashMap<Long, Boolean>();
+			}
+		} catch(IOException i) {
+			return new HashMap<Long, Boolean>();
+		} catch(ClassNotFoundException c) {
+			return new HashMap<Long, Boolean>();
+		}
+	}
 
 	@SubscribeEvent
 	public final void onLogin(EntityJoinWorldEvent e) {
-		Boolean identifier = not_done.get(e.world.getSeed());
+		Boolean identifier = getNot_Done().get(e.world.getSeed());
+		FMLLog.info("[PathFinder] onLogin triggered");
 		if (identifier == null || identifier) {
+			long was = e.world.getSeed();
 			e.world.setRandomSeed((int )(Math. random() * 100 + 1), (int )(Math. random() * 100 + 1), (int )(Math. random() * 100 + 1));
 			long seed = e.world.getSeed();
-			not_done.put(seed, false);
+			getNot_Done().put(seed, false);
+			FMLLog.info("[PathFinder] randomized seed. Seed was: " + was + ". Seed is now: " + seed);
+			try
+			{
+				FileOutputStream fileOut = new FileOutputStream("/pathfinder/not_done.ser");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(e);
+				out.close();
+				fileOut.close();
+				FMLLog.info("[PathFinder] Saved not_done hashmap");
+			} catch(IOException i)
+			{
+				i.printStackTrace();
+			}
 		}
 	}
 
