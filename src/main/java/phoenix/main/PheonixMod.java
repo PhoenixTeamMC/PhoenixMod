a14deb87476d7e56e697ad132e8567d24c14853f
Strikingwolf <Strikingwolf2012@gmail.com>
2015-05-04 15:40:36 -0500

    Nuking filter, PathFinder, and Rain
    * Deleted Filter
    * Deleted Pathfinder
    * Deleted Rain

--- src/main/java/phoenix/main/PheonixMod.java
+++ src/main/java/phoenix/main/PheonixMod.java
@@ -9,12 +9,9 @@ import net.minecraftforge.common.config.Configuration;
 import org.apache.logging.log4j.Logger;
 
 import phoenix.command.CommandInit;
-import phoenix.filter.Filter;
 import phoenix.lib.GuiHandler;
 import phoenix.main.lib.Reference;
 import phoenix.main.proxy.IProxy;
-import phoenix.pathfinder.Pathfinder;
-import phoenix.rain.RainInit;
 import phoenix.rp.Renascintis;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -26,19 +23,16 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES)
 public class PheonixMod {
-	
+
 	public static final Map<IIntializer, Boolean> modules = new HashMap<IIntializer, Boolean>(); //List of modules, to be initialized later
-	
+
 	static{ //Use this to add a module.
-		modules.put(new Filter(), true);
 		modules.put(new Renascintis(), true);
 		modules.put(new CommandInit(), true);
-		modules.put(new RainInit(), true);
-		modules.put(new Pathfinder(), true);
 	}
-	
+
 	Configuration config;
-	
+
 	@Instance(Reference.MODID)
 	public static PheonixMod instance;
 
@@ -46,36 +40,36 @@ public class PheonixMod {
     public static IProxy proxy;
 
 	public static final PheonixCreativeTab creativeTab = new PheonixCreativeTab();
-	
+
 	public Logger log;
-	
+
 	@EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        
+
 		log = event.getModLog();
-		
+
 		config = new Configuration(event.getSuggestedConfigurationFile());
-		
+
 		config.load();
-		
+
 		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
-		
+
 		for(IIntializer module : modules.keySet()){
-			
+
 			boolean shouldInit = config.get("Modules", module.getModuleName(), modules.get(module)).getBoolean();
-			
+
 			if(shouldInit){
 				module.preInit(event);
 			}
-			
+
 			modules.put(module, shouldInit);
-			
+
 		}
-		
+
 		config.save();
-		
+
     }
-    
+
 	@EventHandler
 	public void init(FMLInitializationEvent event){
 		for(IIntializer module : modules.keySet()){
