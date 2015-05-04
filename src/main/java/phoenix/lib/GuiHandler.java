94b642610a3fba63763237c62f72d2aed744a3ef
Strikingwolf <Strikingwolf2012@gmail.com>
2015-05-04 16:52:12 -0500

    Fixed

--- src/main/java/phoenix/lib/GuiHandler.java
+++ src/main/java/phoenix/lib/GuiHandler.java
@@ -1,26 +1,17 @@
 package phoenix.lib;
 
+import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
-import phoenix.filter.TileEntityFilter;
-import phoenix.filter.gui.FilterContainer;
-import phoenix.filter.gui.FilterGui;
-import cpw.mods.fml.common.network.IGuiHandler;
 
-public class GuiHandler implements IGuiHandler
-{
-
-	//Will work on this later, just getting Filters Working
-	
+public class GuiHandler implements IGuiHandler {
 	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		return new FilterContainer(player, (TileEntityFilter) world.getTileEntity(x, y, z));
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		return null;
 	}
 
 	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		return new FilterGui(new FilterContainer(player, (TileEntityFilter) world.getTileEntity(x, y, z)));
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		return null;
 	}
 }
